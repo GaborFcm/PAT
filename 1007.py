5338131bae0a5fe52034d159397e8890842e9270
GaborFcm <1208708534@qq.com>
2017-02-12 12:57:28 +0800

    second commit

--- 1007.py
+++ 1007.py
@@ -1,29 +1,19 @@
 # _*_ coding:utf-8 _*_
 import math
 number = input()
-def prime_number(x):
-    j = 0
+def is_prime(x):
     end_num = int(math.sqrt(x))
     for i in xrange(2, end_num+1):
         if x % i == 0:
-            break
-        else:
-            j = i
-    if j == end_num:
-        return x
-    else:
-        return 1
+            return False
+    return True
 
-prime_num, count = [], 0
-if number > 4:
-    prime_num = [2, 3]
-    for z in xrange(5, number+1, 2):
-        prime_num.append(prime_number(z))
-    prime_num = list(set(prime_num))
-    if 1 in prime_num:
-        prime_num.remove(1)
-    prime_num.sort()
-    for k in xrange(len(prime_num)-1):
-        if prime_num[k+1] - prime_num[k] == 2:
-            count += 1
+z_pre, count = 2, 0
+if number > 2:
+    for z in xrange(3, number+1, 2):
+        if is_prime(z):
+            if z - z_pre == 2:
+                count += 1
+            z_pre = z
 print count
+
