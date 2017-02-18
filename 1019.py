c281af54ed0f067ee3121a490a1c2f481e208182
GaborFcm <1208708534@qq.com>
2017-02-18 10:45:28 +0800

    second commit

--- 1019.py
+++ 1019.py
@@ -2,11 +2,7 @@
 def rank(x):
     while len(x) < 4:
         x = '0' + x
-    for i in xrange(len(x)):
-        for j in xrange(i+1, len(x)):
-            if int(x[i]) < int(x[j]):
-                x = x[:i]+x[j]+x[i+1:j]+x[i]+x[j+1:]
-
+    x = ''.join(sorted(x))
     return x
 n = raw_input()
 while len(n) < 4:
@@ -15,7 +11,7 @@ k = n
 while k != 6174:
     n = rank(str(k))
     m = n[::-1]
-    k = int(n)-int(m)
-    print '%04d - %04d = %04d' % (int(n), int(m), k)
+    k = int(m)-int(n)
+    print '%04d - %04d = %04d' % (int(m), int(n), k)
     if k == 0:
         break
